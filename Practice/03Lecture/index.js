e8e78352b06873a622b8cbcb18bcf64a3106752c
Harsh-Vaishnani <harshvaishnani8@gmail.com>
2024-10-07 19:44:47 +0530

    Node.Js

--- Practice/03Lecture/index.js
+++ Practice/03Lecture/index.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+const fs = require("fs");
+
+// writeFile no use nvi file creat krva mate thay ch
+fs.writeFileSync("./text.txt", "Hello Harsh For Sync.");
+
+//readFile no use data read krva mate thay ch
+const res = fs.readFileSync("./text.txt", "utf-8");
+console.log(res);
+
+//file ne unlink krva mate
+fs.unlinkSync("./text.txt");
+
+//append file ka use data ko add karne ke liye hota hay
+const newData = "\nWelcome to Append Sync in Node.js.";
+fs.appendFileSync("./text.txt", newData);
+
+//rename
+fs.renameSync("./text.txt", "./Harsh.txt");
